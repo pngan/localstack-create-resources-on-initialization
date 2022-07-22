9a7d985388bf2f93fa041822ee7841682da55740
Phillip Ngan <phillip.ngan@gmail.com>
2022-07-22 22:31:38 +1200

    Added S3 buckets

--- shell_script_approach/init-scripts/02-resources-creation.sh
+++ shell_script_approach/init-scripts/02-resources-creation.sh
@@ -8,3 +8,13 @@ aws sqs create-queue --endpoint-url=http://localhost:4566 --queue-name=local_que
 
 echo "########### Listing SQS ###########"
 aws sqs list-queues --endpoint-url=http://localhost:4566 --profile=localstack
+
+
+echo "########### Creating S3 ###########"
+
+aws s3api create-bucket  --endpoint-url=http://localhost:4566 --bucket my-bucket1 --profile=localstack
+aws s3api create-bucket  --endpoint-url=http://localhost:4566 --bucket my-bucket2 --profile=localstack
+
+
+echo "########### Listing S3 ###########"
+aws s3api list-buckets --endpoint-url=http://localhost:4566 --profile=localstack
